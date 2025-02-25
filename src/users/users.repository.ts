34ede82d8437d2c2b87fff7b5667170113c9f843
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-25 19:15:20 -0300

    feat(users): add users repository with create and findByEmail methods

---
+++ src/users/users.repository.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class UsersResitory {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data: createUserDto,
+    });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+}
