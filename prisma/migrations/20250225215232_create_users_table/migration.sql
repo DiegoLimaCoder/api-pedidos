573ed3e6a30d004627ea6aca74933de1d01b9923
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-25 18:57:41 -0300

    feat(users): add users table with email verification and refresh token

---
+++ prisma/migrations/20250225215232_create_users_table/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "emailVerified" BOOLEAN NOT NULL DEFAULT false,
+    "emailVerificationToken" TEXT,
+    "emailVerificationExpires" TIMESTAMP(3),
+    "refreshToken" TEXT,
+    "refreshTokenExpires" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_refreshToken_key" ON "users"("refreshToken");
