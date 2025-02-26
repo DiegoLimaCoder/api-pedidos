fcc29f186f49a10ea9ef49456d5a04a5f8194b25
Diego Lima <diegogoncalvesreisdelima21@gmail.com>
2025-02-26 04:25:14 -0300

    feat(prisma): setup global prisma module
    - Create global Prisma module for database access
    - Add PrismaService as global provider
    - Import PrismaModule in UsersModule
    - Remove redundant PrismaService provider from UsersModule
    - Configure module exports for database access

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 import { UsersResitory } from './users.repository';
 import { MailModule } from 'src/mail/mail.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [MailModule],
+  imports: [MailModule, PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, UsersResitory, PrismaService],
+  providers: [UsersService, UsersResitory],
 })
 export class UsersModule {}
